81ac85becde05a558392b3cc6990e7fa0a1eacf1
Dan Chimento <danchimento@live.com>
2020-04-19 22:15:47 -0400

    Score numbers and starting instructions

--- Scene1.js
+++ Scene1.js
@@ -3,27 +3,35 @@ export default class Scene1 extends Phaser.Scene {
         super({ key: "scene 1" });
 
         // This shouldn't change
-        this.ballImageSize  = 100;
+        this.ballImageSize = 100;
         this.joeImageWidth = 50;
         this.joeImageHeight = 100;
-        
+
         this.ball = null;
         this.ballInitialRotation = 1;
         this.ballMinimumRotation = 30;
         this.ballMaximumRotation = 300;
         this.ballScale = .75;
-        this.ballStartOffset = 30;
+        this.ballStartOffset = 10;
         this.ballStartY = 100;
-        
+
         this.joe = null;
         this.joeScale = .75;
-        
+
         this.cursors = null;
         this.groundHeight = 100;
 
         this.scoreText = null;
         this.highscoreText = null;
         var muted = false;
+
+        this.fontFamily = 'ponde';
+        this.fontColor = "#000";
+        this.textStyle = { color: this.fontColor, fontFamily: this.fontFamily }
+
+        this.gameStarted = false;
+
+        this.birdSpeed = 60;
 
         this.resetGame();
     }
@@ -38,6 +46,14 @@ export default class Scene1 extends Phaser.Scene {
         this.gravity = 400;
         this.beatHighscore = false;
         this.hittingTimer = 0;
+    }
+
+    startGame() {
+        this.ball.setGravityY(this.gravity);
+        this.gameStarted = true;
+        this.ball.setAngularVelocity(this.ballMinimumRotation);
+        this.bird.setVelocityX(-this.birdSpeed);
+        this.gameStartText.setVisible(false);
     }
 
     incrementDifficulty() {
@@ -77,16 +93,14 @@ export default class Scene1 extends Phaser.Scene {
 
     createBall() {
         this.ball = this.physics.add.sprite(this.sys.game.config.width / 2 + this.ballStartOffset, this.ballStartY, 'ball', 0);
-        this.ball.setGravityY(this.gravity);
         this.ball.setCircle(this.ballImageSize / 2);
         this.ball.setBounce(this.ballBounciness);
-        this.ball.setAngularVelocity(this.ballMinimumRotation);
         this.ball.setScale(this.ballScale);
     }
 
     createPlayer() {
         this.joe = this.physics.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height - ((this.groundHeight) + (this.joeImageHeight * this.joeScale / 2)), 'joe_stand');
-        
+
         this.anims.create({
             key: 'joe_stand',
             repeat: 0,
@@ -122,16 +136,15 @@ export default class Scene1 extends Phaser.Scene {
 
     createScenery() {
         this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'background');
-        this.bird = this.physics.add.sprite(this.sys.game.config.width - 100, 100, 'bird');
+        this.bird = this.physics.add.sprite(this.sys.game.config.width + 100, 100, 'bird');
         var flyAnimation = this.anims.create({
             key: 'fly',
             frameRate: 2,
             repeat: -1,
             frames: this.anims.generateFrameNames('bird')
         });
-        
+
         this.bird.anims.load(flyAnimation);
-        this.bird.setVelocityX(-60);
         this.bird.anims.play('fly');
     }
 
@@ -161,12 +174,15 @@ export default class Scene1 extends Phaser.Scene {
     }
 
     createScoreText() {
-        let textStyle = { color: '#000', fontFamily: 'ponde' }
-        this.scoreText = this.add.text(10, 10, "Score: 0", textStyle);
-        this.scoreText.color = "black";
-        this.highscoreText = this.add.text(this.sys.game.config.width - 190, 10, "Highscore: " + localStorage.getItem('highscore'), textStyle);
+        this.scoreText = this.add.text(10, 10, "Score: 0", this.textStyle);
+        this.highscoreText = this.add.text(this.sys.game.config.width - 190, 10, "Highscore: " + localStorage.getItem('highscore'), this.textStyle);
         this.highscoreText.width = 400;
         this.highscoreText.setAlign("right");
+    }
+    
+    createGameStartText() {
+        this.gameStartText = this.add.text(this.sys.game.config.width / 2 - 150, this.sys.game.config.height / 2 - 50, "Keep the ball in the air\n\nPress any key to start", this.textStyle);
+        this.gameStartText.setAlign("center");
     }
 
     onBounce() {
@@ -201,11 +217,14 @@ export default class Scene1 extends Phaser.Scene {
             if (!this.beatHighscore) {
                 this.scoreSound.play();
                 this.beatHighscore = true;
+                this.createFloatingText(this.joe.x, this.joe.y - this.joe.height / 2, "Highscore!", 0x013220, 2);
             }
         }
 
         this.bounceSound.play();
         this.hittingTimer = 10;
+
+        this.createFloatingText(this.joe.x, this.joe.y - this.joe.height / 2, '+1', 0x000000, 1);
     }
 
     saveHighscore(score) {
@@ -213,19 +232,31 @@ export default class Scene1 extends Phaser.Scene {
         localStorage.setItem('highscore', score);
     }
 
+    createFloatingText(x, y, message, color, level) {
+        let animation = this.add.text(x - (message.length * 6), y, message, { fontFamily: this.fontFamily }).setTint(color);
+        this.add.tween({
+            targets: animation,
+            duration: 750,
+            ease: 'Exponential.In',
+            y: y - (50 * level),
+            onComplete: () => {
+                animation.destroy();
+            }, callbackScope: this
+        });
+    }
+
     create() {
         console.log("Creating...");
 
         this.highscore = localStorage.getItem('highscore');
-        if (this.highscore == null)
-        {
+        if (this.highscore == null) {
             this.highscore = 0;
         }
-        
+
         this.cursors = this.input.keyboard.createCursorKeys();
         this.pointer = this.input.activePointer;
 
-        this.input.on('pointerdown', (pointer) => { 
+        this.input.on('pointerdown', (pointer) => {
             this.pointer = pointer;
         });
 
@@ -235,17 +266,27 @@ export default class Scene1 extends Phaser.Scene {
         this.createBounds();
         this.createSounds();
         this.createScoreText();
-        
+        this.createGameStartText();
+
         this.physics.add.collider(this.ball, this.joe, () => this.onBounce());
 
         var muted = localStorage.getItem('muted');
-        if (muted == null)
-        {
+        if (muted == null) {
             this.setMute(false);
         } else {
             this.muted = muted == "true";
             this.setMute(this.muted);
         }
+
+        this.input.keyboard.on("keydown", () => {
+            if (!this.gameStarted) {
+                this.startGame();
+            }
+        });
+
+        this.input.keyboard.on("keydown_M", () => {
+            this.setMute(!this.sound.mute)
+        });
 
         this.input.keyboard.on("keydown_M", () => {
             this.setMute(!this.sound.mute)
@@ -258,33 +299,38 @@ export default class Scene1 extends Phaser.Scene {
     }
 
     update() {
-        this.joe.setVelocity(0);
+        if (this.gameStarted) {
 
-        this.hittingTimer--;
-        var runAnimation = this.hittingTimer <= 0 ? 'joe_run' : 'joe_run_hit';
-        var standAnimation = this.hittingTimer <= 0 ? 'joe_stand' : 'joe_hit';
+            this.joe.setVelocity(0);
 
-        if (this.cursors.left.isDown) {
-            this.joe.setVelocityX(-this.joeSpeed);
-            this.joe.anims.play(runAnimation, true);
-            this.joe.flipX = true;
-        } else if (this.cursors.right.isDown) {
-            this.joe.setVelocityX(this.joeSpeed);
-            this.joe.anims.play(runAnimation, true);
-            this.joe.flipX = false;
+            this.hittingTimer--;
+            var runAnimation = this.hittingTimer <= 0 ? 'joe_run' : 'joe_run_hit';
+            var standAnimation = this.hittingTimer <= 0 ? 'joe_stand' : 'joe_hit';
+
+            if (this.cursors.left.isDown) {
+                this.joe.setVelocityX(-this.joeSpeed);
+                this.joe.anims.play(runAnimation, true);
+                this.joe.flipX = true;
+            } else if (this.cursors.right.isDown) {
+                this.joe.setVelocityX(this.joeSpeed);
+                this.joe.anims.play(runAnimation, true);
+                this.joe.flipX = false;
+            } else {
+                this.joe.anims.play(standAnimation, true);
+            }
+
+            if (this.ball.body.angularVelocity > this.ballMinimumRotation) {
+                this.ball.body.angularVelocity--;
+            }
+
+            if (this.bird.x < -500) {
+                this.bird.x = this.sys.game.config.width + 100;
+            }
+
+            this.scoreText.text = `Score: ${this.score}`
+            this.highscoreText.text = `Highscore: ${this.highscore}`
         } else {
-            this.joe.anims.play(standAnimation, true);
+            this.gameStartText.setVisible((new Date()).getSeconds() % 2 == 0);
         }
-
-        if (this.ball.body.angularVelocity > this.ballMinimumRotation) {
-            this.ball.body.angularVelocity--;
-        }
-
-        if (this.bird.x < -500) {
-            this.bird.x = this.sys.game.config.width + 100;
-        }
-
-        this.scoreText.text = `Score: ${this.score}`
-        this.highscoreText.text = `Highscore: ${this.highscore}`
     }
 }
